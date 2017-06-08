183743200d8e482be0a61fbcde408a14a10c33e5
Olivier Robert <robby57@gmail.com>
2017-06-08 13:42:05 +0200

    add templates, bootstrap, moment

--- hello.py
+++ hello.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, render_template 
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent') 
-    response = make_response('You\'re browser is <p>{}</p>'.format(user_agent))
-    response.set_cookie('answer', '42')
-    return response
+    return render_template('index.html', user_agent=user_agent)
+
+@app.route('/time')
+def time():
+    return render_template('time.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run() 
